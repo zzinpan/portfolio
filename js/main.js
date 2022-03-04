fd9cc876f9356d7cd5c2c85515d8254ade18a315
신명철 <12y8e@naver.com>
2022-03-04 15:32:41 +0900

    -

--- js/main.js
+++ js/main.js
@@ -1,20 +1,42 @@
 $(function(){
 	
-	var $headerButton = $(".header-button");
-	$headerButton.on("click", function(){
+	/**
+	 * 헤더 클릭 이벤트
+	 */
+	var $headerItem = $(".header-item");
+	$headerItem.on("click", function(){
 		
 		var $this = $(this);
 		$this.find("a").get(0).click();
 		
 	});
 	
-	$headerButton.find("a").on("click", function( e ){
+	$headerItem.find("a").on("click", function( e ){
 		
 		e.stopPropagation();
 		
 	});
 	
+	var $headerXsButton = $("#header-xs-button");
+	var $headerXsList = $(".header-xs-list");
+	$headerXsButton.on("click", function( e ){
+		
+		e.stopPropagation();
+		$headerXsList.toggleClass( "show" );
+		
+	});
 	
+	var $body = $("body");
+	$body.on("click", function(){
+		
+		$headerXsList.removeClass( "show" );
+		
+	});
+	
+	
+	/**
+	 * 시뮬레이션 로직 상세 보기 클릭 이벤트
+	 */
 	var $simulationDescButton = $("#simulation-desc-button");
 	var $simulationDesc = $("#simulation-desc");
 	
